d3908088ea84b0a8e37e94af6f04d69f84798e95
sietino86 <sietino86@gmail.com>
2022-09-26 15:38:29 -0300

    javaScript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log ("un texto")
+console.log (45)
+console.log ("mañana es", 4+2 , "de enero")
+
+document.write ("hola A todos desde  js")
+
+let num1 = parseFloat(prompt("Ingrese primer numero"))
+let num2 = parseFloat(prompt("ingrese segundo numero"))
+
+let suma = num1 + num2
+console.log (num1 + num2)
